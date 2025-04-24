0061d83eff2205ca5d08d3d074906c571c5c2c68
MonigaBala <monigabala1207@gmail.com>
2025-04-24 21:22:26 +0530

    Multilingual Commit

--- SamProject/project/src/i18n/i18n.ts
+++ SamProject/project/src/i18n/i18n.ts
@@ -2,11 +2,17 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+// Dynamically import all language JSON files
 import enTranslation from './locales/en.json';
 import taTranslation from './locales/ta.json';
 import hiTranslation from './locales/hi.json';
+import teTranslation from './locales/te.json'; // Add other languages as necessary
+import mlTranslation from './locales/ml.json';
+import knTranslation from './locales/kn.json';
+import frTranslation from './locales/fr.json';
+import deTranslation from './locales/de.json';
 
-// For the MVP, we'll only include three languages, but more can be added
+// Set up the resources for each language
 const resources = {
   en: {
     translation: enTranslation
@@ -16,6 +22,21 @@ const resources = {
   },
   hi: {
     translation: hiTranslation
+  },
+  te: {
+    translation: teTranslation
+  },
+  ml: {
+    translation: mlTranslation
+  },
+  kn: {
+    translation: knTranslation
+  },
+  fr: {
+    translation: frTranslation
+  },
+  de: {
+    translation: deTranslation
   }
 };
 
